74f962c82dc8ed5c538bc03b981c68823df4c55f
Craig <watson236@gmail.com>
2016-05-17 10:55:42 +0000

    root

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var app = express();
+
+app.set('views', __dirname);
+app.set('view engine', 'jade');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('*', function(req, res) {
+    res.render('index');
+});
+
+//app.listen(3000);
+
+app.listen(process.env.PORT, process.env.IP);
