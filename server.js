4703d51ea365acad5bcd4cd473cd2cf3badee14d
Craig <watson236@gmail.com>
2016-05-17 23:22:09 +0000

    added remote mongodb

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var jobModel = require('./models/Job');
 
 var app = express();
 
@@ -7,10 +9,26 @@ app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/api/jobs', function(req, res) {
+    mongoose.model('Job').find({}).exec(function(error, collection) {
+        res.send(collection);
+    });
+});
+
 app.get('*', function(req, res) {
     res.render('index');
 });
 
 //app.listen(3000);
+
+//mongoose.connect('mongodb://localhost/jobfinder');
+mongoose.connect('mongodb://psdev:psdev@ds041613.mlab.com:41613/jobfinder');
+
+var con = mongoose.connection;
+
+con.once('open', function() {
+    console.log('connected to mongodb successfully.');
+    jobModel.seedJobs();
+});
 
 app.listen(process.env.PORT, process.env.IP);
